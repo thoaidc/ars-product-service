4843b757ce263e7cb47921247c0e53de53576cb3
thoaidc <dcthoai1023@gmail.com>
2025-08-29 23:09:42 +0700

    init

---
+++ src/test/java/com/ars/productservice/ArsProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ars.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
